7c3e27ce11ad7c64712dd27d830710daa8ebe80f
BenSagir <bensagir.sbr@gmail.com>
2022-08-24 14:33:09 +0300

    disabe navigaiton header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import Iconio from 'react-native-vector-icons/Ionicons';
 import MainScreen from './App/MainScreen';
 import ScoreScreen from './App/ScoreScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { Store } from './App/redux/store';
@@ -15,7 +13,7 @@ const App = () => {
   return (
     <Provider store={Store}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={MainScreen} />
           <Stack.Screen name="Result" component={ScoreScreen} />
         </Stack.Navigator>
